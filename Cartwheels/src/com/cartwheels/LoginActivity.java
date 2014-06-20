16f8812e2fec09ecaca9afe221c4c63f95492851
ezchen <echen2015@gmail.com>
2014-06-19 21:16:32 -0400

    Changed register activity to a ScrollView. Changed the xml, supports profile pictures (but gets deleted when the user turns his phone orientation)

--- Cartwheels/src/com/cartwheels/LoginActivity.java
+++ Cartwheels/src/com/cartwheels/LoginActivity.java
@@ -1,10 +1,23 @@
 package com.cartwheels;
 
+import java.io.IOException;
+
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,9 +26,11 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.savagelook.android.UrlJsonAsyncTask;
+
 public class LoginActivity extends Activity {
 
-	private final static String LOGIN_API_ENDPOINT_URL = "http://10.0.2.2:3000/api/v1/sessions.json";
+	private final static String LOGIN_API_ENDPOINT_URL = "http://cartwheels.us/mobile/sessions.json";
 	
 	private SharedPreferences preferences;
 	private String userEmail;
@@ -71,7 +86,7 @@ public class LoginActivity extends Activity {
 	    if (userEmail.length() == 0 || userPassword.length() == 0) {
 	        // input fields are empty
 	        Toast.makeText(this, "Please complete all the fields",
-	            Toast.LENGTH_LONG).show();
+	            Toast.LENGTH_SHORT).show();
 	        return;
 	    } else {
 	        LoginTask loginTask = new LoginTask(LoginActivity.this);
@@ -93,7 +108,82 @@ public class LoginActivity extends Activity {
 	}
 	
 	private class LoginTask extends UrlJsonAsyncTask {
-		
+		 public LoginTask(Context context) {
+		        super(context);
+		    }
+
+	    @Override
+	    protected JSONObject doInBackground(String... urls) {
+	        DefaultHttpClient client = new DefaultHttpClient();
+	        HttpPost post = new HttpPost(urls[0]);
+	        JSONObject holder = new JSONObject();
+	        JSONObject userObj = new JSONObject();
+	        String response = null;
+	        JSONObject json = new JSONObject();
+
+	        try {
+	            try {
+	                // setup the returned values in case
+	                // something goes wrong
+	                json.put("success", false);
+	                json.put("info", "Something went wrong. Retry!");
+	                // add the user email and password to
+	                // the params
+	                userObj.put("email", userEmail);
+	                userObj.put("password", userPassword);
+	                holder.put("user", userObj);
+	                StringEntity se = new StringEntity(holder.toString());
+	                post.setEntity(se);
+
+	                // setup the request headers
+	                post.setHeader("Accept", "application/json");
+	                post.setHeader("Content-Type", "application/json");
+
+	                ResponseHandler<String> responseHandler = new BasicResponseHandler();
+	                response = client.execute(post, responseHandler);
+	                json = new JSONObject(response);
+
+	            } catch (HttpResponseException e) {
+	                e.printStackTrace();
+	                Log.e("ClientProtocol", "" + e);
+	                json.put("info", "Email and/or password are invalid. Retry!");
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	                Log.e("IO", "" + e);
+	            }
+	        } catch (JSONException e) {
+	            e.printStackTrace();
+	            Log.e("JSON", "" + e);
+	        }
+
+	        return json;
+	    }
+
+	    @Override
+	    protected void onPostExecute(JSONObject json) {
+	        try {
+	            if (json.getBoolean("success")) {
+	                // everything is ok
+	                SharedPreferences.Editor editor = preferences.edit();
+	                // save the returned auth_token into
+	                // the SharedPreferences
+	                editor.putString("AuthToken", json.getJSONObject("data").getString("auth_token"));
+	                editor.commit();
+
+	                // launch the HomeActivity and close this one
+	                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+	                startActivity(intent);
+	                finish();
+	            }
+	            Toast.makeText(context, json.getString("info"), Toast.LENGTH_SHORT).show();
+	        } catch (Exception e) {
+	            // something went wrong: show a Toast
+	            // with the exception message
+	            Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+	        } finally {
+	            super.onPostExecute(json);
+	        }
+	    }
 	}
 	/*
 	 * Login fragment
--- Cartwheels/src/com/cartwheels/RegisterActivity.java
+++ Cartwheels/src/com/cartwheels/RegisterActivity.java
@@ -1,17 +1,39 @@
 package com.cartwheels;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class RegisterActivity extends Activity {
 
+	private final static String REGISTER_API_ENDPOINT_URL = "http://10.0.2.2:3000/api/v1/registrations";
+	
+	private SharedPreferences preferences;
+	private String userEmail;
+	private String userName;
+	private String userPassword;
+	private String userPasswordConfirmation;
+	
+	/* Photo capturing variables */
+	static final int REQUEST_IMAGE_CAPTURE = 1;
+	String currentPhotoPath;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -49,6 +71,54 @@ public class RegisterActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	public void register(View view) {
+		try {
+			createImageFile("hello");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/*
+	 * Methods used to handle the picture capture
+	 */
+	public void takePicture(View view) {
+	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+	        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+	    }
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+	        Bundle extras = data.getExtras();
+	        Bitmap imageBitmap = (Bitmap) extras.get("data");
+	        ImageView imageView = (ImageView) findViewById(R.id.defaultProfilePicture);
+	        imageView.setImageBitmap(Bitmap.createScaledBitmap(imageBitmap, 125, 125, false));
+	    }
+	}
+	
+	/*
+	 * Create the image
+	 */
+	private File createImageFile(String username) throws IOException {
+	    // Create an image file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    String imageFileName = "com_cartwheels_" + timeStamp + "_" + username;
+	    File storageDir = Environment.getExternalStoragePublicDirectory(
+	            Environment.DIRECTORY_PICTURES);
+	    File image = File.createTempFile(
+	        imageFileName,  /* prefix */
+	        ".jpg",         /* suffix */
+	        storageDir      /* directory */
+	    );
+
+	    // Save a file: path for use with ACTION_VIEW intents
+	    currentPhotoPath = "file:" + image.getAbsolutePath();
+	    return image;
+	}
 
 	/*
 	 * fragment used for people who want to register as a user
